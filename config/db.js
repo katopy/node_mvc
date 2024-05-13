4def63dc91458869c69720541d7a305dce751b1a
kath <84346755+Katopy@users.noreply.github.com>
2024-05-12 23:39:29 -0600

    db config with env variables and sequelize orm

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config({path: '.env'})
+
+const db = new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, process.env.DB_PASS ?? '', {
+    host: process.env.DB_HOST,
+    port: '3306',
+    dialect: 'mysql',
+    define: {
+        timestamps: true
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    operatorAliases: false
+
+});
+
+export default db;
