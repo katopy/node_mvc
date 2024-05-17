346afd9069410b1471fded930af39f1ad9a74bf2
kath <84346755+Katopy@users.noreply.github.com>
2024-05-17 01:09:50 -0600

    middlewares for the app

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // const express = require('express') --> CommonJS
 import express from 'express'
 import userRoutes from './routes/userRoutes.js'
+import db from './config/db.js'
 
 //Crear la aplicacion
 const app = express()
+
+// Habilitar lectura de datos de formularios
+app.use(express.urlencoded({extended: true}))
+
+// Conexion a la base de datos
+try{
+    await db.authenticate();
+    db.sync()
+    console.log('Conexión correcta a la base de datos')
+
+} catch(error){
+    console.log(error)
+}
 
 // Habilitar Pug
 app.set('view engine', 'pug')
@@ -12,10 +26,8 @@ app.set('views', './views')
 // Carpeta publica
 app.use(express.static('public'))
 
-// Routing
+// Routing - cualquier solicitud que comience con /auth será manejada por las rutas definidas en userRoutes.
 app.use('/auth', userRoutes)
-
-
 
 //Definir puerto y arrancar la app
 const port = 3000;
