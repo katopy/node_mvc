aad98825e37e86d1b3b2b3999c80d90ecef8f8e5
kath <84346755+Katopy@users.noreply.github.com>
2024-05-19 13:05:46 -0600

    adding helpers such as tokens and emails

--- helpers/emails.js
+++ helpers/emails.js
@@ -1,15 +1,32 @@
 import nodemailer from 'nodemailer'
-
+import dotenv from 'dotenv'
+dotenv.config({path: '.env'})
 
 const emailRegister = async (data) => {
     const transport = nodemailer.createTransport({
-        host: EMAIL_HOST,
-        port: EMAIL_PORT,
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth: {
-          user: EMAIL_USER,
-          pass: EMAIL_PASS
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
         }
     });
+
+    const { email, name, token } = data
+
+    // Enviar el email
+    await transport.sendMail({
+        from: 'RealEstate.com',
+        to: email,
+        subject: 'Confirm your account in RealEstate',
+        text: 'Confirm your account to proceed',
+        html:
+        `
+        <p>Hi ${name}, your account is almost ready, you just need to confirm it with the link:
+        <a href="">Confirm account</a> </p>
+        `
+
+    })
 }
 
 export{
