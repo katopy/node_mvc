af449327a1a240d882c959dec4399a77f1b34a1c
Katopy <katherinesanchez5858@gmail.com>
2024-05-19 17:28:40 -0600

    changing mysql port to 33061

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config({path: '.env'})
 const db = new Sequelize(process.env.DB_NAME, 
     process.env.DB_USER, process.env.DB_PASS ?? '', {
     host: process.env.DB_HOST,
-    port: '3306',
+    port: '33061',
     dialect: 'mysql',
     define: {
         timestamps: true
