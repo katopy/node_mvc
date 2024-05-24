1db792115e1a04163402bf938c5b449f8e690b28
Katopy <katherinesanchez5858@gmail.com>
2024-05-23 23:19:09 -0600

    changing csurf to csrf-csrf package

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // const express = require('express') --> CommonJS
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import csurf from 'csurf'
+import { doubleCsrf } from 'csrf-csrf'
 import userRoutes from './routes/userRoutes.js'
 import db from './config/db.js'
 
 //Crear la aplicacion
 const app = express()
 
+// csrf configuration
+const { doubleCsrfProtection, generateToken } = doubleCsrf({
+    getSecret: () => "YourSecret", // Replace with your own function to get a secret
+  
+  });
+
+// Habilitar cookie parser
+app.use( cookieParser())
+
+app.use(doubleCsrfProtection);
+
+  
 // Habilitar lectura de datos de formularios
 app.use(express.urlencoded({extended: true}))
+
+// Habilitar CSRF
+//app.use(csurf({cookie: true}))
 
 // Conexion a la base de datos
 try{
@@ -30,7 +48,7 @@ app.use(express.static('public'))
 app.use('/auth', userRoutes)
 
 //Definir puerto y arrancar la app
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`)
 
