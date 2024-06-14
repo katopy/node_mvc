95b8f9c4ea803c67c85d5fb78ee3670bb6768135
Katopy <katherinesanchez5858@gmail.com>
2024-06-14 11:17:43 -0600

    adding the post route to send the log in form

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { formularioLogin, formularioRegister, register, confirm, confirmToken, newPassword, formularioForgotPassword, resetPassword, } from "../controllers/usuarioController.js";
+import { formularioLogin, authenticate, formularioRegister, register, confirm, confirmToken, newPassword, formularioForgotPassword, resetPassword, } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 //Routing (diferentes endpoints que soporta nuestra app)
+// To log in
 router.get('/login', formularioLogin)
+router.post('/login', authenticate)
 
+// To register
 router.get('/register', formularioRegister)
 router.post('/register', register)
 
@@ -14,12 +17,10 @@ router.post('/forgot-password', resetPassword)
 
 // Luego de enviar el correo, pagina para confirmar cuenta
 // Routing dinamico --> controller
-
 router.get('/confirm/:token', confirm)
 
 // Mostrar al abrir el link
-
-router.get('/forgot-password:/token', confirmToken)
-router.post('/forgot-password:/token', newPassword)
+router.get('/forgot-password/:token', confirmToken)
+router.post('/forgot-password/:token', newPassword)
 
 export default router
