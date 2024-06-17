1ba984610a852e6f1d3803076fbf6931d63d9115
Katopy <katherinesanchez5858@gmail.com>
2024-06-17 15:33:02 -0600

    creating JWT tokens

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -1,5 +1,11 @@
+import jwt from 'jsonwebtoken'
+
+const generateJWT = data => jwt.sign({id: data.id, name: data.name}, process.env.JWT_KEY, {expiresIn: '1d'}) 
+
+
 const generateId = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
 
 export {
-    generateId
+    generateId,
+    generateJWT
 }
