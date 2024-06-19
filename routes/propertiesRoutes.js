665941ae1dbc569dca50d1d6abf290585e3dbb4a
Katopy <katherinesanchez5858@gmail.com>
2024-06-19 13:57:07 -0600

    adding routes for properties

--- routes/propertiesRoutes.js
+++ routes/propertiesRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {admin} from '../controllers/propertyController.js'
+import {admin, create} from '../controllers/propertyController.js'
 
 const router = express.Router();
 
 router.get('/my-properties', admin)
+router.get('/properties/create', create)
 
 
 export default router
