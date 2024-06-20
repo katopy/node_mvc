50c84a7675e7ed8e2a67a18a42da5efa848a4e16
Katopy <katherinesanchez5858@gmail.com>
2024-06-19 18:18:17 -0600

    get lat and lng from the pin

--- src/js/map.js
+++ src/js/map.js
@@ -4,6 +4,9 @@
     const lng = -89.2199982;
     const map = L.map('map').setView([lat, lng ], 10);
     let marker;
+
+    // Use Provider and Geocoder
+    const geocodeService = L.esri.Geocoding.geocodeService();
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,6 +19,20 @@
         autoPan: true
     })
     .addTo(map)
+
+    // Get the info of the marker (lat and lng)
+    marker.on('moveend', function(e){
+        marker = e.target
+        const position = marker.getLatLng()
+        // Center based where the user left the pin
+        map.panTo(new L.LatLng(position.lat, position.lng))
+
+        // Name of street
+        geocodeService.reverse().latlng(position, 13).run(function(error, result){
+        marker.bindPopup(result.address.LongLabel)
+        })
+
+    })
 
     
 })()
